38abb9c032deb6bc5903e93e4b41f1e08c303328
Akku Iki Alie <achmadali1992@yahoo.com>
2016-12-19 13:00:42 +0700

    add air conditioning

---
+++ AirConditioning.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Short description for AirConditioning.php.
+ *
+ * @author Akku Iki Alie <achmadali1992@yahoo.com>
+ *
+ * @version 0.1
+ *
+ * @copyright (C) 2016 Akku Iki Alie <achmadali1992@yahoo.com>
+ * @license Shoop E-Commerce License
+ */
+class AirConditioning
+{
+    public $jenis;
+    public $warna;
+
+    public function nyalakan()
+    {
+        // your statement
+    }
+
+    public function matikan()
+    {
+        // your statement
+    }
+}
